7994096de6bd1131ada21a862604f1988ac71a7d
razoplite <56459654+razoplite@users.noreply.github.com>
2020-07-21 22:53:42 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 
 
-var radius = 240;
+var radius = 320;
 var autoRotate = true;
 var rotateSpeed = -60;
-var imgWidth = 120; 
-var imgHeight = 170;
+var imgWidth = 483; 
+var imgHeight = 346;
 
 
 var bgMusicURL = 'https://api.soundcloud.com/tracks/143041228/stream?client_id=587aa2d384f7333a886010d5f52f302a';
